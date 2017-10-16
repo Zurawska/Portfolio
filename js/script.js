37dfdf1ac3ec8c3dbebfac291c4bb82bb5811d99
Edyta <edyta.zurawska8@gmail.com>
2017-10-16 20:22:59 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,87 @@
+// $( document ).ready(function() {
+//
+//     $(".line").on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
+//         function() {
+//             var interval = 500;
+//             $("circle").each(function(index){
+//                 $(this).delay(interval).hide(900);
+//                 interval += 500;
+//         });
+//     });
+// });
+
+
+// function bigScreenAnimation() {
+//     console.log("COAKHKA")
+//     var interval = 500;
+//
+//     if ($(window).width() > 1200) {
+//         $(".pink_flower").attr({display:"none"});
+//
+//         $("circle").each(function(index){
+//             $(this).delay(interval).fadeIn(500);
+//             interval += 500;
+//         });
+//
+//         $(".key-anim6").on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
+//             function() {
+//                 var interval = 500;
+//                 $("circle").each(function(index){
+//                     $(this).delay(interval).hide(900);
+//                     interval += 500;
+//                 });
+//                 var interval = 500;
+//                 $(".flower").each(function(index){
+//                     $(this).delay(interval).fadeIn(1000);
+//                     interval += 500;
+//                 });
+//         });
+
+    // } else {
+
+        // console.log("COAKHKAsadasdasds")
+        // $("circle").css("r",10);
+        // $(".flower").attr({width:"50px", height:"50px"});
+        // $(".line, .flower, circle").attr({display:"none"});
+
+        // var interval = 100;
+        // $(".pink_flower").each(function(index){
+        //     $(this).css();
+        //     interval += 100;
+        // });
+        // var interval = 500;
+        // $(".slogan").each(function(index){
+        //     $(this).delay(interval).slideDown(1000);
+        //     interval += 500;
+        // });
+
+        //
+        // var interval = 1000;
+        // $(".flower:nth-of-type(3), .flower:nth-of-type(2), .flower:nth-of-type(6), .flower:nth-of-type(4)").each(function(index){
+        //     $(this).delay(interval).show(1000);
+        //     interval += 1000;
+        // });
+        //
+        // $(".fl1").attr({width:"100px", height:"100px"});
+        // // $(".fl2").css(cy, 5);
+        //
+        // $("circle").each(function(index){
+        //     $(this).delay(interval).show(500);
+        //     interval += 500;
+        // });
+        //
+        // $("circle").each(function(index){
+        //     $(this).delay(interval).hide(900);
+        //     interval += 500;
+        // });
+        // $(".flower").each(function(index){
+        //     $(this).delay(interval).fadeIn(1000);
+        //     interval += 500;
+        // });
+
+
+//     }
+// }
+//
+// $(document).ready(bigScreenAnimation);
+// $(window ).resize(bigScreenAnimation);
